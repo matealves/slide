cf952ca921562a45aab603490d815bf3685d42ba
Mateus Alves <contatomateusalves@hotmail.com>
2023-03-02 11:16:37 -0300

    feat: nav buttons prev and next

--- js/Slide.js
+++ js/Slide.js
@@ -1,6 +1,6 @@
 import debounce from "./debounce.js";
 
-export default class Slide {
+export class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
@@ -158,11 +158,33 @@ export default class Slide {
   }
 
   init() {
-    this.bindEvents("onStart", "onMove", "onEnd");
+    this.bindEvents(
+      "onStart",
+      "onMove",
+      "onEnd",
+      "activePrevSlide",
+      "activeNextSlide"
+    );
     this.transition(true);
     this.addSlideEvents();
     this.slidesConfig();
     this.addResizeEvent();
+    this.changeSlide(3);
     return this;
+  }
+}
+
+// ################################
+
+export class SlideNav extends Slide {
+  addArrow(prev, next) {
+    this.prevElement = document.querySelector(prev);
+    this.nextElement = document.querySelector(next);
+    this.addArrowEvent();
+  }
+
+  addArrowEvent() {
+    this.prevElement.addEventListener("click", this.activePrevSlide);
+    this.nextElement.addEventListener("click", this.activeNextSlide);
   }
 }
